06c1ae8213c8967c8c43e760df32b1666cb0fa18
David Gaspar <davidgaspar.dev@gmail.com>
2022-02-28 00:03:59 +0000

    Checking arguments

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 use std::collections::HashMap;
 
 pub const IMAGE_PATH: &'static str = "--imagepath";
@@ -14,7 +15,7 @@ pub fn get_arguments() -> Result<HashMap<String, String>, String> {
 
     let args: Vec<String> = env::args().collect();
 
-    return load_args(args);
+    return load_args(args)
 }
 
 fn load_args(args: Vec<String>) -> Result<HashMap<String, String>, String> {
@@ -24,13 +25,21 @@ fn load_args(args: Vec<String>) -> Result<HashMap<String, String>, String> {
 
     for i in 1..(args_number + 1) {
         let index = usize::from((i * 2) - 1);
+
+        // Checking arg key
         let ref argkey = &args[index];
-        if is_argkey_valid(*argkey) {
-            let ref argvalue = &args[index + 1];
-            setting.insert((*argkey).to_string(), (*argvalue).to_string());
-        } else {
+        if !is_argkey_valid(*argkey) {
             return Err(String::from("Arg key invalid"));
         }
+
+        // Checking arg value
+        let ref argvalue = &args[index + 1];
+        if !is_argvalue_valid(*argkey, *argvalue) {
+            return Err("Arg value invalid".to_string())
+        }
+
+        // Add in the setting
+        setting.insert((*argkey).to_string(), (*argvalue).to_string());
     }
 
     for &argkey in POSSIBLE_ARGS {
@@ -53,5 +62,24 @@ fn is_argkey_valid(argkey: &String) -> bool {
         }
     }
 
-    return false;
+    false
+}
+
+fn is_argvalue_valid(argkey: &String, argvalue: &String) -> bool {
+    if argkey == IMAGE_PATH {
+        if !Path::new(argvalue).exists() || !is_path_imagefile(argvalue) {
+            return false
+        }
+    }
+    if argkey == FLUTTER_PATH {
+        if !Path::new(argvalue).exists() {
+            return false
+        }
+    }
+    true
+}
+
+fn is_path_imagefile(argvalue: &String) -> bool {
+    let path_ext: String = argvalue.chars().skip(argvalue.len() - 4).collect();
+    path_ext == ".png" || path_ext == ".jpg" || path_ext == "jpeg"
 }

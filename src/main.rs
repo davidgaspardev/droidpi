3c72ec3f55333db9e02a5f0c794fe9d70cafbb7b
David Gaspar <davidgaspar.dev@gmail.com>
2022-03-06 02:57:30 +0000

    Loading image via path argument - close #2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate image;
 
+use image::GenericImageView;
+
 mod cli;
 
 fn main() {
@@ -9,6 +11,14 @@ fn main() {
 
     match result {
         Err(error) => println!("{}", error),
-        _ => (),
+        Ok(setting) => {
+            println!("setting: {:?}", setting);
+            println!("setting[IMAGE_PATH]: {}", setting[cli::IMAGE_PATH]);
+            println!("setting[FLUTTER_PATH]: {}", setting[cli::FLUTTER_PATH]);
+
+            let img = image::open(format!("{}", setting[cli::IMAGE_PATH])).unwrap();
+
+            println!{"Dimensions: {:?}", img.dimensions()};
+        }
     }
 }

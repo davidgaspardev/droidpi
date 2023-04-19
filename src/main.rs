6c3c22bd7815e1bb49d37ba2848556d2b31dee25
David Gaspar <davidgaspar.dev@gmail.com>
2023-04-19 15:24:17 -0300

    chore: use resize object

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate image;
 
-use image::GenericImageView;
-
 mod cli;
+mod resize;
 
 fn main() {
     println!("Welcome to droiDPI");
@@ -15,10 +14,23 @@ fn main() {
             println!("setting: {:?}", setting);
             println!("setting[IMAGE_PATH]: {}", setting[cli::IMAGE_PATH]);
             println!("setting[FLUTTER_PATH]: {}", setting[cli::FLUTTER_PATH]);
+            println!("setting[IMAGE_NAME]: {}", setting[cli::IMAGE_NAME]);
 
-            let img = image::open(format!("{}", setting[cli::IMAGE_PATH])).unwrap();
+            let image_path = &setting[cli::IMAGE_PATH];
+            let flutter_path = &setting[cli::FLUTTER_PATH];
+            let image_name = &setting[cli::IMAGE_NAME];
 
-            println!{"Dimensions: {:?}", img.dimensions()};
+            match image::open(image_path) {
+                Err(err) => println!("{}", err),
+                Ok(img ) => {
+                    /* Resize image to flutter suport */
+                    let r = resize::Resize::new(img, image_name.to_string());
+                    r.create_xxhdpi();
+                    r.create_xhdpi();
+                    r.create_hdpi();
+                    r.create_mdpi();
+                }
+            }
         }
     }
 }

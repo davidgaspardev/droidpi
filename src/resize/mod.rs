a3ca85b3da5eaf082eb8f71f608d6acca1887047
David Gaspar <davidgaspar.dev@gmail.com>
2023-06-12 21:38:58 -0300

    chore: implement functions to create images

--- src/resize/mod.rs
+++ src/resize/mod.rs
@@ -1,4 +1,5 @@
 use image::{DynamicImage, GenericImageView};
+use std::fs;
 
 const FACTOR_XXXHDPI: f32 = 4.0;
 const FACTOR_XXHDPI: f32 = 3.0;
@@ -9,53 +10,90 @@ const FACTOR_MDPI: f32 = 1.0;
 pub struct Resize {
     xxxhdpi_img: DynamicImage,
     name: String,
+    width: u32,
+    height: u32,
 }
 
 impl Resize {
     pub fn new(xxxhdpi_image: DynamicImage, name: String) -> Self {
+        let (width, height) = xxxhdpi_image.dimensions();
+        
         Resize {
             xxxhdpi_img: xxxhdpi_image,
-            name: name
+            name,
+            width,
+            height
         }
     }
 
-    pub fn create_xxhdpi(&self) {
-        let (width, height) = self.xxxhdpi_img.dimensions();
+    pub fn create_xxxdhpi(&self, directory: &String) {
+        if !fs::metadata(&directory).is_ok() {
+            match fs::create_dir(&directory) {
+                Ok(_) => println!("Directory created successfully: {}", directory),
+                Err(err) => println!("Failed to create directory: {}", err),
+            }
+        }
+        
+        self.xxxhdpi_img.save(format!("{}/{}.png", directory, self.name)).unwrap();
+    }
 
-        let new_width = (((width as f32) * FACTOR_XXHDPI) / FACTOR_XXXHDPI) as u32;
-        let new_height = (((height as f32) * FACTOR_XXHDPI) / FACTOR_XXXHDPI) as u32;
+    pub fn create_xxhdpi(&self, directory: &String) {
+        if !fs::metadata(&directory).is_ok() {
+            match fs::create_dir(&directory) {
+                Ok(_) => println!("Directory created successfully: {}", directory),
+                Err(err) => println!("Failed to create directory: {}", err),
+            }
+        }
+
+        let new_width = (((self.width as f32) * FACTOR_XXHDPI) / FACTOR_XXXHDPI) as u32;
+        let new_height = (((self.height as f32) * FACTOR_XXHDPI) / FACTOR_XXXHDPI) as u32;
 
         let xxhdpi_image = self.xxxhdpi_img.resize(new_width, new_height, image::imageops::FilterType::Nearest);
-        xxhdpi_image.save(format!("data/{}@3.0x.png", self.name)).unwrap();
+        xxhdpi_image.save(format!("{}/{}.png", directory, self.name)).unwrap();
     }
 
-    pub fn create_xhdpi(&self) {
-        let (width, height) = self.xxxhdpi_img.dimensions();
+    pub fn create_xhdpi(&self, directory: &String) {
+        if !fs::metadata(&directory).is_ok() {
+            match fs::create_dir(&directory) {
+                Ok(_) => println!("Directory created successfully: {}", directory),
+                Err(err) => println!("Failed to create directory: {}", err),
+            }
+        }
 
-        let new_width = (((width as f32) * FACTOR_XHDPI) / FACTOR_XXXHDPI) as u32;
-        let new_height = (((height as f32) * FACTOR_XHDPI) / FACTOR_XXXHDPI) as u32;
+        let new_width = (((self.width as f32) * FACTOR_XHDPI) / FACTOR_XXXHDPI) as u32;
+        let new_height = (((self.height as f32) * FACTOR_XHDPI) / FACTOR_XXXHDPI) as u32;
 
         let xhdpi_image = self.xxxhdpi_img.resize(new_width, new_height, image::imageops::FilterType::Nearest);
-        xhdpi_image.save(format!("data/{}@2.0x.png", self.name)).unwrap();
+        xhdpi_image.save(format!("{}/{}.png", directory, self.name)).unwrap();
     }
 
-    pub fn create_hdpi(&self) {
-        let (width, height) = self.xxxhdpi_img.dimensions();
+    pub fn create_hdpi(&self, directory: &String) {
+        if !fs::metadata(&directory).is_ok() {
+            match fs::create_dir(&directory) {
+                Ok(_) => println!("Directory created successfully: {}", directory),
+                Err(err) => println!("Failed to create directory: {}", err),
+            }
+        }
 
-        let new_width = (((width as f32) * FACTOR_HDPI) / FACTOR_XXXHDPI) as u32;
-        let new_height = (((height as f32) * FACTOR_HDPI) / FACTOR_XXXHDPI) as u32;
+        let new_width = (((self.width as f32) * FACTOR_HDPI) / FACTOR_XXXHDPI) as u32;
+        let new_height = (((self.height as f32) * FACTOR_HDPI) / FACTOR_XXXHDPI) as u32;
 
         let hdpi_image = self.xxxhdpi_img.resize(new_width, new_height, image::imageops::FilterType::Nearest);
-        hdpi_image.save(format!("data/{}@1.5x.png", self.name)).unwrap();
+        hdpi_image.save(format!("{}/{}.png", directory, self.name)).unwrap();
     }
 
-    pub fn create_mdpi(&self) {
-        let (width, height) = self.xxxhdpi_img.dimensions();
+    pub fn create_mdpi(&self, directory: &String) {
+        if !fs::metadata(&directory).is_ok() {
+            match fs::create_dir(&directory) {
+                Ok(_) => println!("Directory created successfully: {}", directory),
+                Err(err) => println!("Failed to create directory: {}", err),
+            }
+        }
 
-        let new_width = (((width as f32) * FACTOR_MDPI) / FACTOR_XXXHDPI) as u32;
-        let new_height = (((height as f32) * FACTOR_MDPI) / FACTOR_XXXHDPI) as u32;
+        let new_width = (((self.width as f32) * FACTOR_MDPI) / FACTOR_XXXHDPI) as u32;
+        let new_height = (((self.height as f32) * FACTOR_MDPI) / FACTOR_XXXHDPI) as u32;
 
         let mdpi_image = self.xxxhdpi_img.resize(new_width, new_height, image::imageops::FilterType::Nearest);
-        mdpi_image.save(format!("data/{}@1.0x.png", self.name)).unwrap();
+        mdpi_image.save(format!("{}/{}.png", directory, self.name)).unwrap();
     }
 }

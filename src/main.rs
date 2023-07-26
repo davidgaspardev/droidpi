68c03f3b2f92c72daafd1f419bf6a958a4a9b384
David Gaspar <davidgaspar.dev@gmail.com>
2023-07-26 19:53:40 -0300

    refactor: user more if let pattern

--- src/main.rs
+++ src/main.rs
@@ -11,46 +11,43 @@ fn main() {
 
     let result = cli::get_arguments();
 
-    match result {
-        Err(error) => println!("{}", error),
-        Ok(setting) => {
-            println!("setting: {:?}", setting);
-            println!("setting[IMAGE_PATH]: {}", setting[cli::IMAGE_PATH]);
-            println!("setting[FLUTTER_PATH]: {}", setting[cli::FLUTTER_PATH]);
-            println!("setting[IMAGE_NAME]: {}", setting[cli::IMAGE_NAME]);
+    if let Err(err) = result {
+        eprintln!("{}", err);
+        return
+    }
 
-            let image_path = &setting[cli::IMAGE_PATH];
-            let flutter_path = &setting[cli::FLUTTER_PATH];
-            let image_name = &setting[cli::IMAGE_NAME];
+    if let Ok(setting) = result {
+        println!("setting: {:?}", setting);
+        println!("setting[IMAGE_PATH]: {}", setting[cli::IMAGE_PATH]);
+        println!("setting[FLUTTER_PATH]: {}", setting[cli::FLUTTER_PATH]);
+        println!("setting[IMAGE_NAME]: {}", setting[cli::IMAGE_NAME]);
 
-            match image::open(image_path) {
-                Err(err) => println!("{}", err),
-                Ok(img ) => {
-                    /* Resize image to flutter suport */
-                    let r: resize::Resize = resize::Resize::new(img, image_name.to_string());
+        let image_path = &setting[cli::IMAGE_PATH];
+        let flutter_path = &setting[cli::FLUTTER_PATH];
+        let image_name = &setting[cli::IMAGE_NAME];
 
-                    let all_path = [
-                        format!("{}", flutter_path),
-                        format!("{}/1.5x", flutter_path),
-                        format!("{}/2.0x", flutter_path),
-                        format!("{}/3.0x", flutter_path),
-                        format!("{}/4.0x", flutter_path)
-                    ];
-                    
+        if let Ok(img) = image::open(image_path) {
+            let img_resize = resize::Resize::new(img, image_name.to_string());
 
-                    match fs::create_dir_all(flutter_path) {
-                        Ok(_) => {
-                            r.create_mdpi(&all_path[0]);
-                            r.create_hdpi(&all_path[1]);
-                            r.create_xhdpi(&all_path[2]);
-                            r.create_xxhdpi(&all_path[3]);
-                            r.create_xxxdhpi(&all_path[4]);
-                        },
-                        Err(e) => match e.kind() {
-                            ErrorKind::AlreadyExists => println!("Directory already exists: {}", flutter_path),
-                            _ => panic!("Error creating directory: {}", e),
-                        }
-                    }
+            let all_path = [
+                format!("{}", flutter_path),
+                format!("{}/1.5x", flutter_path),
+                format!("{}/2.0x", flutter_path),
+                format!("{}/3.0x", flutter_path),
+                format!("{}/4.0x", flutter_path)
+            ];
+
+            match fs::create_dir_all(flutter_path) {
+                Ok(_) => {
+                    img_resize.create_mdpi(&all_path[0]);
+                    img_resize.create_hdpi(&all_path[1]);
+                    img_resize.create_xhdpi(&all_path[2]);
+                    img_resize.create_xxhdpi(&all_path[3]);
+                    img_resize.create_xxxdhpi(&all_path[4]);
+                },
+                Err(err) => match err.kind() {
+                    ErrorKind::AlreadyExists => eprintln!("Directory already exists: {}", flutter_path),
+                    _ => panic!("Error creating directory: {}", err)
                 }
             }
         }

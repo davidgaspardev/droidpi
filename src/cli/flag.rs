112f61e12b16b0ef8d6237e4560f97c52d2e00ac
David Gaspar <davidgaspar.dev@gmail.com>
2025-08-01 02:24:20 -0300

    refactor: Create a Flag enum to handle with flags

---
+++ src/cli/flag.rs
@@ -0,0 +1,66 @@
+pub enum Flag {
+    Src,
+    OutDir,
+    Name,
+    Platform,
+}
+
+impl Flag {
+    pub fn from_str(flag_str: &String) -> Option<Flag> {
+        if Flag::Src.is_equal(flag_str) {
+            return Some(Flag::Src);
+        }
+
+        if Flag::OutDir.is_equal(flag_str) {
+            return Some(Flag::OutDir);
+        }
+
+        if Flag::Name.is_equal(flag_str) {
+            return Some(Flag::Name);
+        }
+
+        if Flag::Platform.is_equal(flag_str) {
+            return Some(Flag::Platform);
+        }
+
+        None
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Flag::Src => "--src",
+            Flag::OutDir => "--outDir",
+            Flag::Name => "--name",
+            Flag::Platform => "--platform",
+        }
+    }
+
+    pub fn get_all_required() -> [&'static str; 4] {
+        return [
+            Flag::Src.as_str(),
+            Flag::OutDir.as_str(),
+            Flag::Name.as_str(),
+            Flag::Platform.as_str(),
+        ];
+    }
+
+    pub fn is_equal(&self, flag: &String) -> bool {
+        self.as_str() == flag
+    }
+
+    pub fn is_src(flag: &String) -> bool {
+        Flag::Src.is_equal(flag)
+    }
+
+    pub fn is_out_dir(flag: &String) -> bool {
+        Flag::OutDir.is_equal(flag)
+    }
+
+    pub fn is_name(flag: &String) -> bool {
+        Flag::Name.is_equal(flag)
+    }
+
+    pub fn is_platform(flag: &String) -> bool {
+        Flag::Platform.is_equal(flag)
+    }
+}

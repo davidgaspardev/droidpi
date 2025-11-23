b5cdae9dc4bf78930d9b71f1366f3c3760263eb5
David Gaspar <davidgaspar.dev@gmail.com>
2025-11-23 15:41:49 -0300

    feat: Add UseDrawable support

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,4 +1,4 @@
-use crate::resize::Resize;
+use crate::{context::Context, resize::Resize};
 
 mod android;
 mod flutter;
@@ -23,5 +23,5 @@ impl PlatformFactory {
 }
 
 pub trait Platform {
-    fn create_images(&self, out_dir: &str) -> Result<(), String>;
+    fn create_images(&self, ctx: &Context) -> Result<(), String>;
 }
